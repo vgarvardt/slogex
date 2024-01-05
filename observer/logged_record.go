52767392d7392fff9aa3bbd5cfe3540415ef52a5
Vladimir Garvardt <vgarvardt@gmail.com>
2024-01-05 22:04:20 +0100

    feat: observer extension and initial GHA workflows

---
+++ observer/logged_record.go
@@ -0,0 +1,28 @@
+package observer
+
+import "log/slog"
+
+// An LoggedRecord is a log record representation suitable for direct comparison.
+// Record attributes are extracted to a list to allow comparing them.
+type LoggedRecord struct {
+	Record slog.Record
+	Attrs  []slog.Attr
+}
+
+// AttrsMap returns a map for all attributes in the log record.
+// Groups are recursively converted to maps.
+func (e LoggedRecord) AttrsMap() map[string]any {
+	return e.attrsMap(e.Attrs)
+}
+
+func (e LoggedRecord) attrsMap(attrs []slog.Attr) map[string]any {
+	res := make(map[string]any, len(attrs))
+	for _, a := range attrs {
+		if a.Value.Kind() == slog.KindGroup {
+			res[a.Key] = e.attrsMap(a.Value.Group())
+			continue
+		}
+		res[a.Key] = a.Value.Any()
+	}
+	return res
+}
